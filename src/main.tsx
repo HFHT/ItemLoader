32021c450dce43dd2126b74fea5383c6fa1cfae7
Art <44448694+HFHT@users.noreply.github.com>
2023-09-02 17:34:24 -0700

    Build Fix 2

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+// import App from './App.tsx'
 import './index.css'
-import { AuthContextProvider } from './context/AuthContextProvider.tsx'
+// import { AuthContextProvider } from './context/AuthContextProvider.tsx'
+import App from './App'
+import { AuthContextProvider } from './context'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <AuthContextProvider msal={{}} schema={{}} selects={{}}>
